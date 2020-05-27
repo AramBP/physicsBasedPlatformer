67029c49bf4c9356263b3e06453177ba3cb211f2
AramBP <61063343+AramBP@users.noreply.github.com>
2020-05-27 09:45:46 +0200

    Add files via upload

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public Transform castPoint;//bepaald in de inspector waar de raycasting begint
+
+	public Transform player;//bepaald ook in de inspector waar de player zich bevindt
+
+	public float agroRange;//bepaald de grote van de raycast
+
+	public float moveSpeed;//bepaald de snelheid van de enemey
+
+	Rigidbody2D rb2d;//rigidbody component
+
+	private bool isSearching;//checkt of die niet aan het zoeken is
+
+	bool isFacingLeft = false;//kijkt of we naar links kijken
+	private bool isAgro = false;//kijkt of we de speler hebben gezien
+
+	//zorgt dat we starten met de rigidbody
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*//afstand naar speler
+		 * zonder raycast un_comment dit en comment de andere functie 
+		 
+		float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+		if (distToPlayer < agroRange) {
+
+			ChasePlayer ();
+
+		} else {
+			StopChasingPlayer ();
+		}
+		*/
+		//pathfinding met raycast
+		if (CanSeePlayer (agroRange)) 
+		{
+			isAgro = true;
+			} else {
+			if (isAgro == true) {
+				if (!isSearching) {
+					isSearching = true;
+					Invoke ("StopChasingPlayer", 3);
+				}
+			}
+		}
+
+		if (isAgro) {
+			ChasePlayer ();
+		}
+	}
+
+	//checkt of we speler hwbbwn gezien
+	bool CanSeePlayer(float distance)
+	{
+		bool val = false;
+		float castDist = distance;
+
+		if (isFacingLeft) 
+		{
+			castDist = -distance;
+		}
+
+		Vector2 endPos = castPoint.position + Vector3.right * castDist;
+		RaycastHit2D hit = Physics2D.Linecast(castPoint.position, endPos, 1 << LayerMask.NameToLayer("Action"));
+
+		if (hit.collider != null) {
+			if (hit.collider.gameObject.CompareTag ("Player")) {
+				val = true;
+			} else {
+				val = false;
+			} 
+
+			Debug.DrawLine (castPoint.position, hit.point, Color.red);
+
+		} else {
+			Debug.DrawLine (castPoint.position, endPos, Color.red);
+		}
+
+		return val;
+	}
+
+	//hier checkt hij of wel achter de speler aanzit
+	void ChasePlayer(){
+
+		if (transform.position.x < player.position.x) {
+			rb2d.velocity = new Vector2 (moveSpeed, 0);
+			transform.localScale = new Vector2 (1, 1);
+			isFacingLeft = false;
+
+		} else if (transform.position.x > player.position.x){
+			transform.localScale = new Vector2 (-1, 1);
+			rb2d.velocity = new Vector2 (-moveSpeed, 0);
+			isFacingLeft = true;
+
+		}
+	}
+	//hier checkt hij of hij niet meer achter de speler aanzit
+	void StopChasingPlayer(){
+		isSearching = false;
+		isAgro = false;
+		rb2d.velocity = new Vector2(0,0);
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))] //zorgt dat we de controller2D script nodig hebben om te starten we lenen hier namelijk wat variablene uit
+public class Player : MonoBehaviour {
+
+	//variablen hieronder gaan over springen
+	public float maxJumpHeight = 4;
+	public float minJumpHeight = 1;
+	public float timeToJumpApex = .4f;
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
+	float moveSpeed = 6;
+
+	//variabelen hieronder gaan over op muren springen
+	public Vector2 wallJumpClimb;
+	public Vector2 wallJumpOff;
+	public Vector2 wallLeap;
+
+	//dit gaat ook over muren
+	public float wallSlideSpeedMax = 3;
+	public float wallStickTime = .25f;
+	float timeToWallUnstick;
+
+	//deze gaan over snelheid en meer springen
+	float gravity;
+	float maxJumpVelocity;
+	float minJumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	private bool facingRight;
+
+	//met deze varaibele hebben we een shortcut naar de controller2D script
+	Controller2D controller;
+
+	Vector2 directionalInput;
+	bool wallSliding;
+	int wallDirX;
+
+	//berekent aan het begin de zwaartekracht en de springkracht
+	void Start() {
+		facingRight = true;
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
+		print ("Gravity: " + gravity + "  Jump Velocity: " + maxJumpVelocity);
+	}
+		
+		
+	//hier bereken we wallsliding en snelheid
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis("Horizontal");
+		Flip (horizontal);
+		CalculateVelocity ();
+		HandleWallSliding ();
+
+		controller.Move (velocity * Time.deltaTime, directionalInput);
+
+		if (controller.collisions.above || controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
+				velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.deltaTime;
+			} else {
+				velocity.y = 0;
+			}
+		}
+
+	}
+
+	//meer wallsliding
+	public void SetDirectionalInput(Vector2 input){
+		directionalInput = input;
+	}
+
+	public void OnJumpInputDown(){
+		if (wallSliding) {
+			if (wallDirX == directionalInput.x) {
+				velocity.x = -wallDirX * wallJumpClimb.x;
+				velocity.y = wallJumpClimb.y;
+			}
+			else if (directionalInput.x == 0) {
+				velocity.x = -wallDirX * wallJumpOff.x;
+				velocity.y = wallJumpOff.y;
+			}
+			else {
+				velocity.x = -wallDirX * wallLeap.x;
+				velocity.y = wallLeap.y;
+			}
+		}
+		if (controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
+				if (directionalInput.x != -Mathf.Sign (controller.collisions.slopeNormal.x)) {
+					velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+					velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+				}
+			} else {
+				velocity.y = maxJumpVelocity;
+			}
+		}
+	}
+
+	//voor springen
+	public void OnJumpInputUp(){
+		if (velocity.y > minJumpVelocity) {
+			velocity.y = minJumpVelocity;
+		}
+	}
+		
+	//voor wallsliding
+	void HandleWallSliding(){
+		wallDirX = (controller.collisions.left) ? -1 : 1;
+		wallSliding = false;
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
+			wallSliding = true;
+
+			if (velocity.y < -wallSlideSpeedMax) {
+				velocity.y = -wallSlideSpeedMax;
+			}
+
+			if (timeToWallUnstick > 0) {
+				velocityXSmoothing = 0;
+				velocity.x = 0;
+
+				if (directionalInput.x != wallDirX && directionalInput.x != 0) {
+					timeToWallUnstick -= Time.deltaTime;
+				}
+				else {
+					timeToWallUnstick = wallStickTime;
+				}
+			}
+			else {
+				timeToWallUnstick = wallStickTime;
+			}
+
+		}
+
+	}
+	//het berkene van snelheid zodat we niet op 0.000001 frames per seconde runnen
+	//want de voide update() method berekent eens per frame dus dit bespaart ons een paar kb en wat technische problemen
+	void CalculateVelocity(){
+		float targetVelocityX = directionalInput.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+	}
+
+	private void Flip(float horizontal)
+	{
+		if (horizontal > 0 && !facingRight || horizontal > 0 && facingRight) 
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+			
+		}
+	}
+}
+	
+	
+	
